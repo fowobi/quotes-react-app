4adaf420fbdc583cb732627a1c8557f6c18ef3a0
fowobi <109865505+fowobi@users.noreply.github.com>
2023-08-01 02:02:18 +0100

    update

--- src/RandomQuote.js
+++ src/RandomQuote.js
@@ -1,26 +1,66 @@
+// import React, { useState, useEffect } from "react";
+// import API_URL from "./config";
+
+// function RandomQuote() {
+//   const [quote, setQuote] = useState("");
+
+//   useEffect(() => {
+//     fetch(`${API_URL}/quotes/random`)
+//       .then((response) => response.json())
+//       .then((data) => setQuote(`${data.quote} | ${data.author}`));
+//   }, []);
+
+//   const handleNewQuote = () => {
+//     fetch(`${API_URL}/quotes/random`)
+//       .then((response) => response.json())
+//       .then((data) => setQuote(`${data.quote} | ${data.author}`));
+//   };
+
+//   return (
+//     <div>
+//       <h1 className="r-quote">Random Quote:</h1>
+//       <p className="p-quotes" onClick={handleNewQuote}>
+//         {quote}
+//       </p>
+    
+//     </div>
+//   );
+// }
+
+// export default RandomQuote;
+
+
 import React, { useState, useEffect } from "react";
 import API_URL from "./config";
 
 function RandomQuote() {
   const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
 
   useEffect(() => {
     fetch(`${API_URL}/quotes/random`)
       .then((response) => response.json())
-      .then((data) => setQuote(`${data.quote} | ${data.author}`));
+      .then((data) => {
+        setQuote(data.quote);
+        setAuthor(data.author);
+      });
   }, []);
 
   const handleNewQuote = () => {
     fetch(`${API_URL}/quotes/random`)
       .then((response) => response.json())
-      .then((data) => setQuote(`${data.quote} | ${data.author}`));
+      .then((data) => {
+        setQuote(data.quote);
+        setAuthor(data.author);
+      });
   };
 
   return (
     <div>
       <h1 className="r-quote">Random Quote:</h1>
       <p className="p-quotes" onClick={handleNewQuote}>
-        {quote}
+        {quote} <span className="separator">|</span>{" "}
+        <span className="author">{author}</span>
       </p>
     </div>
   );
