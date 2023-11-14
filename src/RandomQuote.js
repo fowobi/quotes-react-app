52bf153c90f8b2ccca5581114898fa2de758f757
fowobi <109865505+fowobi@users.noreply.github.com>
2023-11-14 10:37:15 +0000

    update

--- src/RandomQuote.js
+++ src/RandomQuote.js
@@ -1,35 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import API_URL from "./config";
-
-// function RandomQuote() {
-//   const [quote, setQuote] = useState("");
-
-//   useEffect(() => {
-//     fetch(`${API_URL}/quotes/random`)
-//       .then((response) => response.json())
-//       .then((data) => setQuote(`${data.quote} | ${data.author}`));
-//   }, []);
-
-//   const handleNewQuote = () => {
-//     fetch(`${API_URL}/quotes/random`)
-//       .then((response) => response.json())
-//       .then((data) => setQuote(`${data.quote} | ${data.author}`));
-//   };
-
-//   return (
-//     <div>
-//       <h1 className="r-quote">Random Quote:</h1>
-//       <p className="p-quotes" onClick={handleNewQuote}>
-//         {quote}
-//       </p>
-    
-//     </div>
-//   );
-// }
-
-// export default RandomQuote;
-
-
 import React, { useState, useEffect } from "react";
 import API_URL from "./config";
 
