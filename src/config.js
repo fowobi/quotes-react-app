0f0809dbeb2a7bbe2b97145d5970fb09e06c5f92
fowobi <109865505+fowobi@users.noreply.github.com>
2024-01-25 12:06:49 +0000

    updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_URL = "https://tosin-quote-server.glitch.me"; // Replace with your actual API URL
+const API_URL = "https://fowobi-quotes-glitch-server.glitch.me"; // Replace with your actual API URL
 
 export default API_URL;
